906fef21981eded542aa989ef53029f69d89809d
David Slimp (rock808) <rock808@David-Slimp.com>
2024-01-30 16:16:09 -0600

    Now using Time::HiRes for game speed control.

--- perlblaster.pl
+++ perlblaster.pl
@@ -1,24 +1,30 @@
 #!/usr/bin/perl -w
 #
-# perlblaster.pl
-#	Perl Defense Blaster
+# perlblaster.pl  -  Perl Defense Blaster
 #		Created on:  Fri Aug 18 14:12:04 CDT 2000
-#		By: David Slimp
+#		Author: David Slimp
 #
-#	This is an attempt at creating an ascii graphic
-#	arcade game fully in perl.
-
+# This is an attempt at creating an ascii graphic arcade game fully in perl.
+#
 
 use Term::ReadKey;
 use Curses;
+use Time::HiRes qw(time sleep);
 use strict;
 
-my $VERSION='0.0.1';
+my $VERSION='0.0.1';	# THIS will need to get updated as we advance to the new stage of code
 my $debug=0;
 
 ##
 ## Initialization
 ##
+
+# Time control variables
+my $enemy_update_interval = 1;  # seconds between each enemy movement
+my $missile_update_interval = .1;  # seconds between each missile movement
+my $last_enemy_update_time = 0;
+my $last_missile_update_time = 0;
+my $current_time = time;
 
 my(@missilex, @missiley, $missileturn, $maxmissiles);
 my($enemyx, $enemyy, $enemyspeed, $showspeed);
@@ -27,15 +33,13 @@ initscr();
 my $key="";
 my $score=0;
 my @termsize=GetTerminalSize;
-my $maxx		= $termsize[0];
+my $maxx		= $termsize[0]-1;
 my $maxy		= $termsize[1];
 my $x		= int($maxx/2);
-my $y		= $maxy-2;
-#$missilex	= $x;
-#$missiley	= $y;
+my $y		= $maxy-3;
 my($tot);
-$missileturn=0;
-$maxmissiles=1;
+$missileturn=0;	# counter for which missile to move next
+$maxmissiles=1;	# maximum number of missiles allowed on screen at once
 
 ReadMode 4;	# non breaking mode
 addstr($y, $x, 'A');
@@ -48,38 +52,48 @@ addstr($y, $x, 'A');
 ##
 
 while ( $key ne "q" ) {
-	while (not defined ($key = ReadKey(-1))) {
+	$current_time = time   ; # get the current time
 
-		if ( @missilex ) { &MoveMissile($missileturn) }
-
-		&MoveEnemy;
-
-		$showspeed=$enemyspeed*10000;
-		$tot=@missilex;
-		addstr($maxy-1, 1, "score: $score    EnemySpeed: $showspeed       $tot  turn=$missileturn    (q = quit)");
-		refresh; 
-			
-	}
+	$key = ReadKey(-1); # read a key without blocking
+	defined $key or $key = ""; # if no key was pressed, set $key to ""
 
 
+	# Update missile based on time
+    if ($current_time - $last_missile_update_time > $missile_update_interval) {
+        if ( @missilex ) { &MoveMissile($missileturn) }
+            $last_missile_update_time = $current_time;
+        }
+
+    # Update enemy based on time
+    if ($current_time - $last_enemy_update_time > $enemy_update_interval) {
+        &MoveEnemy;
+        $last_enemy_update_time = $current_time;
+    }
+
+	$showspeed=$enemyspeed*10;	# FIXME: $enemyspeed might now be used/needed anymore with Time::HiRes
+	$tot=@missilex;
+	addstr($maxy-2, 1, "score: $score    EnemySpeed: $showspeed       $tot  turn=$missileturn    (q = quit)");
+	refresh; 
+	
 	$key eq "4" and $x>0 and $x--;
 	$key eq "6" and $x<$maxx and $x++;
-	#if ($key eq "5" and not $missile) { &StartMissile }
 	if ($key eq "5" and @missilex<$maxmissiles ) { &StartMissile }
-
 	addstr($y, $x-1, ' A ');
+	
 	refresh; 
+	sleep(0.001);
+
 }
 
 ReadMode 0;	# normal mode
-
+system("reset");	# reset the terminal (fixes hiding input characters)
 
 ##
 ## Subroutines
 ##
 
 sub StartMissile {
-	system("play laser.wav &");
+	system("aplay laser.wav  &>/dev/null  &");	# FIXME: We need to find a way to play audio via PERL
 	$missilex[@missilex]=$x;
 	$missiley[@missiley]=$y;
 }
@@ -94,7 +108,7 @@ sub MoveMissile {
 	$debug  and  addstr(3,3,"mnum=$mnum   missileturn=$_[0]")  and   refresh;
 
 	addstr($missiley[$mnum], $missilex[$mnum], ' ');
-	$missiley[$mnum]-=.002;
+	$missiley[$mnum]-=1;
 	addstr($missiley[$mnum], $missilex[$mnum], '!');
 	if ( $missiley[$mnum]<1 ) {
 		&MissileStop($mnum);
@@ -131,13 +145,13 @@ sub MissileStop {
 sub StartEnemy {
 	$enemyy=0;
 	$enemyx=int(rand($maxx));
-	$enemyspeed=($score/30)*.001;
-	$enemyspeed < .001 and $enemyspeed=.001;
+	$enemyspeed=($score/30)*.1;	# FIXME: Might not need $enemyspeed anymore with Time::HiRes
+	$enemyspeed < .1 and $enemyspeed=.1;
 }
 
 sub MoveEnemy {
 	addstr($enemyy, $enemyx, ' ');
-	$enemyy+=$enemyspeed;
+	$enemyy+=1;
 	addstr($enemyy, $enemyx, 'V');
 	if ($enemyy >= $maxy) {
 		addstr($enemyy, $enemyx, 'V');
@@ -150,7 +164,7 @@ sub ExplodeEnemy {
 	my($x,$y) = @_;
 	
 	# FIXME: We need to find a way to play audio via PERL
-	system("play explos.wav &");
+	system("aplay explos.wav &>/dev/null &");	# FIXME: We need to find a way to play audio via PERL
 
 	# FIXME: This should be a more interesting explosion at some point, but should not slow (suspend) the game while the explosion is occuring
 	addstr($y, $x, ' ');
